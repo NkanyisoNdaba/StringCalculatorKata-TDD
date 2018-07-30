a9ace4d465798d96098bc3ee5c6ea523025e7f6d
nkanyiso <ndaba.nkanyi@live.co.uk>
2018-07-30 08:47:46 +0200

    done

---
+++ StringCalclulatorTwo/StringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace StringCalclulatorTwo
+{
+    public class StringCalculator
+    {
+        private int _sum = 0;
+
+        public int Add(string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                numbers = numbers.Replace("//", "");
+            }
+
+            IfNumberIsNegative(numbers);
+
+            var splitNumbers = numbers.Split(new[] { ",", "\n", ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in splitNumbers)
+            {
+                if (NumberIsGreaterThan1000(number))
+                {
+                    _sum += int.Parse(number);
+                }
+            }
+
+            return _sum;
+        }
+
+        private static bool NumberIsGreaterThan1000(string number)
+        {
+            return int.Parse(number) <= 1000;
+        }
+
+        private static void IfNumberIsNegative(string numbers)
+        {
+            if (numbers.Contains("-"))
+            {
+                throw new Exception($"negatives not allowed {numbers}");
+            }
+        }
+    }
+}
---
+++ StringCalclulatorTwo/StrintCalculatorTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringCalclulatorTwo
+{
+    [TestFixture]
+    public class StrintCalculatorTests
+    {
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturnZero()
+        {
+            //Arrange
+            var numbers = string.Empty;
+            int expected = 0;
+            var sut = CreateStringCalculator();
+
+            //Act
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }     
+
+        [Test]
+        public void Add_GivenOne_ShouldReturnOne()
+        {
+            //Arrange
+            var numbers = "1";
+            int expected = 1;
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestCase("1 ,2", 3)]
+        [TestCase("1,2,4,4", 11)]
+        [TestCase("2,3,1,5,54", 65)]
+        public void Add_GivenMultipleNumbers_ShouldReturnTheirSum(string numbers, int expected)
+        {   
+            //Arrange
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Add_GivenNumbersWithNewLinesBetweenThem_ShouldReturnSum()
+        {
+            //Arrange
+            var numbers = "1\n2,3";
+            int expected = 6;
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenNumbersWithDifferentDelimiters_ShouldReturnTheirSum()
+        {
+            //Arrange
+            var numbers = "//;\n1;2";
+            int expected = 3;       
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = sut.Add(numbers);  
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenANegativeNumber_ShouldThrowExceptionAndReturnThatNumber()
+        {
+            //Arrange
+            var numbers = "-10";
+            string expected = $"negatives not allowed {numbers}";       
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = Assert.Throws<Exception>(() => sut.Add(numbers));
+
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [Test]
+        public void Add_GivenMultipleNegativeNumbers_ShouldThrowExceptionAndReturnNumbers()
+        {
+            //Arrange
+            var numbers = "-10,-1,-2,-2";
+            string expected = $"negatives not allowed {numbers}";
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = Assert.Throws<Exception>(() => sut.Add(numbers));
+
+            //Assert
+            Assert.AreEqual(expected, actual.Message);
+        }
+
+        [Test]
+        public void Add_GivenNumbersBiggerThan1000_ShouldIgnoreThem()
+        {
+            //Arrange   
+            var numbers = "1001, 2";    
+            int expected = 2;
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Add_GivenNumbersBetween0And1000_ShouldIgnoreThem()
+        {
+            //Arrange   
+            var numbers = "1000, 2";
+            int expected = 1002;
+            var sut = CreateStringCalculator();
+
+            //Act'
+            var actual = sut.Add(numbers);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static StringCalculator CreateStringCalculator()
+        {
+            return new StringCalculator();
+        }
+    }
+}
